5d4f6a6e7090144274e2f1f5d4592b9a31eb12dd
spence <spencer.d.tenney@gmail.com>
2021-12-03 16:08:01 -0500

    forgot var name change 2

--- main.c
+++ main.c
@@ -396,8 +396,8 @@ void cpb(int *reg, int operand){
       int rbyte = *reg & 0x00FF;
       int temp = rbyte - operand;
       
-      n = (t<0)? 1:0;
-      z = (t<0)? 1:0;
+      n = (temp < 0)? 1:0;
+      z = (temp < 0)? 1:0;
       v = 0;
       c = 0;
 }
